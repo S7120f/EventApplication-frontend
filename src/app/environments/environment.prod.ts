66b9692b4a6568820c5709f67c01fc9088637bb7
unknown <skonby_jonas@hotmail.com>
2025-10-31 04:42:42 +0100

    fixa api base

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://starfish-app-u9vws.ondigitalocean.app/api',
+  apiBaseUrl: 'https://starfish-app-u9vws.ondigitalocean.app',
   wsBaseUrl: 'wss://starfish-app-u9wvs.ondigitalocean.app/ws'
 };
