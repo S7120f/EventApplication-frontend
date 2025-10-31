baa838a2b20bee50f36f7609c32e8ba430bef8b4
unknown <skonby_jonas@hotmail.com>
2025-10-31 05:18:48 +0100

    p

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://starfish-app-u9vws.ondigitalocean.app',
-  wsBaseUrl: 'https://starfish-app-u9vws.ondigitalocean.app/ws'
+  apiBaseUrl: 'https://starfish-app-u9wvs.ondigitalocean.app/',
+  wsBaseUrl: 'https://starfish-app-u9wvs.ondigitalocean.app/'
 };
--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://starfish-app-u9vws.ondigitalocean.app',
-  wsBaseUrl: 'https://starfish-app-u9vws.ondigitalocean.app/ws'
+  apiBaseUrl: 'https://starfish-app-u9wvs.ondigitalocean.app/',
+  wsBaseUrl: 'https://starfish-app-u9wvs.ondigitalocean.app//ws'
 };
