c52707b3276d443cff5eb2ec5c992d25cc6380a7
unknown <skonby_jonas@hotmail.com>
2025-10-31 05:23:04 +0100

    p

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://starfish-app-u9wvs.ondigitalocean.app/',
-  wsBaseUrl: 'https://starfish-app-u9wvs.ondigitalocean.app/'
+  apiBaseUrl: 'https://starfish-app-u9wvs.ondigitalocean.app',
+  wsBaseUrl: 'https://starfish-app-u9wvs.ondigitalocean.app'
 };
--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://starfish-app-u9wvs.ondigitalocean.app/',
-  wsBaseUrl: 'https://starfish-app-u9wvs.ondigitalocean.app//ws'
+  apiBaseUrl: 'https://starfish-app-u9wvs.ondigitalocean.app',
+  wsBaseUrl: 'https://starfish-app-u9wvs.ondigitalocean.app/ws'
 };
