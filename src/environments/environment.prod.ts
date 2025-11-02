df708ddea3c4292bc14b15f8faa942755079613c
unknown <skonby_jonas@hotmail.com>
2025-11-02 06:28:33 +0100

    s

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://shark-app-yvjps.ondigitalocean.app/',
+  apiBaseUrl: 'https://shark-app-yvjps.ondigitalocean.app',
   wsBaseUrl: 'https://shark-app-yvjps.ondigitalocean.app/ws'
 };
